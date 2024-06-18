b6f9cac326d020518072ac81ad9950ea36d90221
zhouyu5 <yu.zhou@intel.com>
2024-06-18 23:03:05 +0800

    update docker run command

--- docker/run.sh
+++ docker/run.sh
@@ -11,16 +11,19 @@ docker run \
     -v /dev/dri:/dev/dri \
     -v $(pwd):/work \
     -p 2222:2222 \
+    -p 11111:11111 \
+    -p 11112:11112 \
+    -p 11113:11113 \
     -v /etc/hosts:/etc/hosts \
     --ipc=host \
+    --network=host \
     -e http_proxy=$http_proxy \
     -e https_proxy=$https_proxy \
     -e no_proxy=$no_proxy \
-    -itd nathanzz2/cpu_pyg:v1
+    -itd nathanzz2/cpu_pyg:v1 \
+&& docker exec -it "pvc_train" bash
 
-
-docker exec -it "pvc_train" bash
-
+docker stop pvc_train && docker rm pvc_train
 docker stop pvc_train && docker rm pvc_train && docker system prune -a -f
 
 
