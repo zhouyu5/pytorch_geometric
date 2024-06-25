822ec711c05a35f1dfe81da4ba04f5444d4290c8
zhouyu5 <yu.zhou@intel.com>
2024-06-25 11:04:32 +0800

    test

--- examples/distributed/pyg/test.sh
+++ examples/distributed/pyg/test.sh
@@ -6,8 +6,7 @@ PYG_WORKSPACE=/work/examples/distributed/pyg
 USER=root
 SSH_PROT=2222
 EXEC_SCRIPT="node_ogb_cpu.py"
-# source /opt/intel/oneapi/setvars.sh --force; 
-CMD="cd ${PYG_WORKSPACE}; python ${EXEC_SCRIPT} --ddp_backend=gloo"
+CMD="source /opt/intel/oneapi/setvars.sh --force; cd ${PYG_WORKSPACE}; python ${EXEC_SCRIPT} --ddp_backend=gloo"
 
 # Node number:
 NUM_NODES=2
