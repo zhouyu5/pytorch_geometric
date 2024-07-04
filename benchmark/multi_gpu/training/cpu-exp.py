e02775364f76931c6291deda98bde81f6e298aa6
zhouyu5 <yu.zhou@intel.com>
2024-07-04 00:28:37 +0000

    update

--- benchmark/multi_gpu/training/cpu-exp.py
+++ benchmark/multi_gpu/training/cpu-exp.py
@@ -1,57 +1,78 @@
 import os
 import sys
+import argparse
 
+
+def excute_command(command, message):
+    os.system(command)
+    print(command)
+    print('#' * 70)
+    print(message)
+    print('#' * 70)
+    sys.stdout.flush()
+    
+
+def get_predefined_args() -> argparse.ArgumentParser:
+    argparser = argparse.ArgumentParser(
+        'GNN distributed (DDP) training benchmark')
+    add = argparser.add_argument
+    add('--hosts', nargs='+')
+    return argparser
+
+
+def main():
+    dataset_list = ['Reddit', 'ogbn-products']
+
+    for host in args.hosts:
+        for dataset in dataset_list:
+            command = f"""
+            ssh {host} \
+            'mpirun \
+                -np 4 \
+                -verbose -prepend-rank -print-rank-map \
+                python training_benchmark_xpu.py \
+                --dataset {dataset} --model sage \
+                --batch-size 128 \
+                --num-layers 2 \
+                --num-hidden-channels 64 \
+                --num-neighbors 10 \
+                --num-epochs 10 \
+                --num-workers 16 \
+                --test \
+                --device cpu'
+            """
+            message = f'np 4 on {host}, dataset {dataset}'
+            excute_command(command, message)
+
+    for host in args.hosts:
+        for dataset in dataset_list:
+            command = f"""
+            ssh {host} \
+            'I_MPI_OFI_PROVIDER=tcp FI_TCP_IFACE=bond0 \
+            mpirun \
+                -genv MASTER_ADDR={host} \
+                -genv MASTER_PORT=11111 \
+                -hosts {args.hosts} \
+                -np 8 -ppn 4  \
+                -verbose -prepend-rank -print-rank-map \
+                python training_benchmark_xpu.py \
+                --dataset {dataset} --model sage \
+                --batch-size 64  \
+                --num-layers 2 \
+                --num-hidden-channels 64 \
+                --num-neighbors 10 \
+                --num-epochs 10 \
+                --num-workers 16 \
+                --test \
+                --device cpu'
+            """
+            message = f'np 8 on {host}, dataset {dataset}'
+            excute_command(command, message)
 
 
 if __name__ == "__main__":
-    dataset_list = ['Reddit', 'ogbn-products']
-
-    for dataset in dataset_list:
-        command = f"""
-        mpirun \
-            -np 4 \
-            -verbose -prepend-rank -print-rank-map \
-            python training_benchmark_xpu.py \
-            --dataset {dataset} --model sage \
-            --batch-size 128 \
-            --num-layers 2 \
-            --num-hidden-channels 64 \
-            --num-neighbors 10 \
-            --num-epochs 10 \
-            --num-workers 16 \
-            --test \
-            --device cpu
-        """
-        os.system(command)
-        print('#' * 70)
-        print(command)
-        print('#' * 70)
-        sys.stdout.flush()
-
-    for dataset in dataset_list:
-        command = f"""
-        I_MPI_OFI_PROVIDER=tcp FI_TCP_IFACE=bond0 \
-        mpirun \
-            -genv MASTER_ADDR=x1002c1s4b0n0 \
-            -genv MASTER_PORT=11111 \
-            -hosts x1001c7s1b0n0,x1002c1s4b0n0 \
-            -np 8 -ppn 4  \
-            -verbose -prepend-rank -print-rank-map \
-            python training_benchmark_xpu.py \
-            --dataset {dataset} --model sage \
-            --batch-size 64  \
-            --num-layers 2 \
-            --num-hidden-channels 64 \
-            --num-neighbors 10 \
-            --num-epochs 10 \
-            --num-workers 16 \
-            --test \
-            --device cpu
-        """
-        os.system(command)
-        print('#' * 70)
-        print(command)
-        print('#' * 70)
-        sys.stdout.flush()
+    argparser = get_predefined_args()
+    args = argparser.parse_args()
+    main()
 
 # python cpu-exp.py 2>&1 | tee -a cpu.log
